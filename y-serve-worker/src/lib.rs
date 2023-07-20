cc6d22c93402e97893b4c1ce05cb701345635c3c
Paul Butler <paul@driftingin.space>
2023-07-20 15:44:14 -0400

    scaffold out cloudflare worker

---
+++ y-serve-worker/src/lib.rs
@@ -0,0 +1,38 @@
+use worker::{durable_object, Env, Request, Result, Response, event, Router};
+
+#[event(fetch)]
+pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Response> {
+    console_error_panic_hook::set_once();
+    let router = Router::new();
+
+    let response = router
+        .get("/", |_, _| Response::ok("Hello world!"))
+        .run(req, env)
+        .await?;
+
+    Ok(response)
+}
+
+#[durable_object]
+pub struct YServe {
+    
+}
+
+
+#[durable_object]
+impl DurableObject for YServe {
+    fn new(state: State, env: Env) -> Self {
+        Self {
+        }
+    }
+
+    async fn fetch(&mut self, req: Request) -> Result<Response> {
+        let url = req.url()?;
+        let (_, path) = url.path().rsplit_once('/').unwrap_or_default();
+        let method = req.method();
+        match (method, path) {
+            _ => Response::error("Document command not found", 404),
+        }
+    }
+}
+
