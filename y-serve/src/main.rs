f7ee0eeadf701e97727db2dc8c3dc73b6a096ecf
Paul Butler <paul@driftingin.space>
2023-07-23 09:43:55 -0400

    refactor dump

--- y-serve/src/main.rs
+++ y-serve/src/main.rs
@@ -1,23 +1,21 @@
 use crate::stores::filesystem::FileSystemStore;
 use anyhow::{anyhow, Result};
 use clap::{Parser, Subcommand};
-use lib0::any::Any;
+use dump::dump;
 use s3::Region;
 use std::{
-    collections::HashMap,
     net::{IpAddr, Ipv4Addr, SocketAddr},
     path::PathBuf,
     sync::Arc,
 };
 use stores::blobstore::S3Store;
 use tracing::metadata::LevelFilter;
-use tracing_subscriber::{
-    prelude::__tracing_subscriber_SubscriberExt, util::SubscriberInitExt, EnvFilter,
-};
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
 use y_serve_core::{auth::Authenticator, doc_connection::DOC_NAME, store::Store, sync_kv::SyncKv};
-use yrs::{types::ToJson, Array, Doc, ReadTxn, Transact};
+use yrs::{Doc, Transact};
 use yrs_kvstore::DocOps;
 
+mod dump;
 mod server;
 mod stores;
 
@@ -133,31 +131,12 @@ async fn main() -> Result<()> {
             let sync_kv = SyncKv::new(Arc::new(store), doc_id, || {}).await?;
             let doc = Doc::new();
 
-            {
-                let mut txn = doc.transact_mut();
-                sync_kv
-                    .load_doc(DOC_NAME, &mut txn)
-                    .map_err(|e| anyhow!("Error loading doc: {:?}", e))?;
+            let mut txn = doc.transact_mut();
+            sync_kv
+                .load_doc(DOC_NAME, &mut txn)
+                .map_err(|e| anyhow!("Error loading doc: {:?}", e))?;
 
-                let root_keys = txn.root_keys();
-
-                let mut map: HashMap<String, Any> = HashMap::new();
-
-                for key in root_keys {
-                    let value = txn.get_array(key).expect("Failed to get array");
-                    if value.len(&txn) > 0 {
-                        map.insert(key.to_string(), value.to_json(&txn));
-                        continue;
-                    }
-
-                    let value = txn.get_map(key).expect("Failed to get map");
-                    map.insert(key.to_string(), value.to_json(&txn));
-                }
-
-                let result = Any::Map(Box::new(map));
-
-                dump_object(&result);
-            }
+            dump(&txn)
         }
         ServSubcommand::GenToken => {
             let key = Authenticator::gen_key()?;
@@ -169,46 +148,4 @@ async fn main() -> Result<()> {
     }
 
     Ok(())
-}
-
-fn dump_object_inner(result: &Any, indent: usize) {
-    let indent_str = "  ".repeat(indent);
-
-    match result {
-        Any::Map(map) => {
-            println!("{{");
-            for (key, value) in map.iter() {
-                print!("  {}{}: ", indent_str, key);
-                dump_object_inner(value, indent + 1);
-            }
-            println!("{}}}", indent_str);
-        }
-        Any::Array(array) => {
-            println!("[");
-            for value in array.iter() {
-                print!("{}  ", indent_str);
-                dump_object_inner(value, indent + 1);
-            }
-            println!("{}]", indent_str);
-        }
-        Any::String(string) => {
-            println!("\"{}\"", string.replace('\"', "\\\""));
-        }
-        Any::Number(number) => {
-            println!("{}", number);
-        }
-        Any::Bool(boolean) => {
-            println!("{}", boolean);
-        }
-        Any::Null => {
-            println!("null");
-        }
-        Any::Undefined => todo!(),
-        Any::BigInt(_) => todo!(),
-        Any::Buffer(_) => todo!(),
-    }
-}
-
-fn dump_object(result: &Any) {
-    dump_object_inner(result, 0);
 }
