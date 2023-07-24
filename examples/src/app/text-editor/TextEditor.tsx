b096dc3a066f6ba418a4908b15a88706ce622007
Felicia <ffeliciachang@gmail.com>
2023-07-24 14:54:23 -0700

    quill binding

--- examples/src/app/text-editor/TextEditor.tsx
+++ examples/src/app/text-editor/TextEditor.tsx
@@ -1,15 +1,36 @@
 "use client"
 
 import { useText } from "@/lib/provider"
-import {useState} from "react"
-import ReactQuill from 'react-quill';
-import 'react-quill/dist/quill.snow.css';
+import { useEffect, useRef } from "react";
+import { QuillBinding } from "y-quill";
+import Quill from 'quill'
 
 import 'quill/dist/quill.bubble.css';
 
 export function TextEditor() {
     const yText = useText('text')
 
+    const editorRef = useRef<HTMLDivElement | null>(null);
+    const bindingRef = useRef<QuillBinding | null>(null)
 
-    return <ReactQuill theme="snow" value={yText?.toString()} onChange={(delta) => yText?.applyDelta(delta)} />;
+    useEffect(() => {
+        console.log(editorRef.current)
+      if (editorRef.current) {
+        const quill = new Quill(editorRef.current, {
+          theme: 'bubble', // or any other Quill theme you prefer
+          modules: {
+            toolbar: [
+              [{ header: [1, 2, false] }],
+              ['bold', 'italic', 'underline', 'strike'],
+              [{ list: 'ordered' }, { list: 'bullet' }],
+              ['link'],
+            ],
+          },
+        });
+  
+        bindingRef.current = new QuillBinding(yText!, quill)
+      }
+    }, [yText]);
+
+    return(<div ref={editorRef}/>)
 }
