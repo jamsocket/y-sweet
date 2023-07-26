cc43ddd8cb1a85aa17cae7a265a0656252b2509b
Paul Butler <paul@driftingin.space>
2023-07-26 14:31:51 -0400

    add API to token env var

--- examples/src/lib/config.ts
+++ examples/src/lib/config.ts
@@ -2,5 +2,5 @@ import { DocumentManagerOptions } from "./yserv";
 
 export const ENV_CONFIG: DocumentManagerOptions = {
     endpoint: process.env.Y_SERVE_ENDPOINT,
-    token: process.env.Y_SERVE_TOKEN,
+    token: process.env.Y_SERVE_API_TOKEN,
 }
