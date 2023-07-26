a97cdddf82e8c6fea24d42585444e8be72fe6839
Felicia <ffeliciachang@gmail.com>
2023-07-26 08:19:40 -0400

    cursors

--- examples/src/app/text-editor/TextEditor.tsx
+++ examples/src/app/text-editor/TextEditor.tsx
@@ -1,23 +1,29 @@
 "use client"
 
-import { useText } from "@/lib/provider"
+import { useText, useAwareness } from "@/lib/provider"
 import { useEffect, useRef } from "react";
 import { QuillBinding } from "y-quill";
 import Quill from 'quill'
+import QuillCursors from 'quill-cursors'
 
 import 'quill/dist/quill.snow.css';
 
 export function TextEditor() {
     const yText = useText('text')
-
+    const awareness = useAwareness()
     const editorRef = useRef<HTMLDivElement | null>(null);
     const bindingRef = useRef<QuillBinding | null>(null)
 
     useEffect(() => {
-      if (editorRef.current) {
+        if (bindingRef.current !== null) {
+            return
+        }
+      if (editorRef.current && awareness) {
+        Quill.register('modules/cursors', QuillCursors);
         const quill = new Quill(editorRef.current, {
           theme: 'snow', // or any other Quill theme you prefer
           modules: {
+            cursors: true,
             toolbar: [
               [{ header: [1, 2, false] }],
               ['bold', 'italic', 'underline', 'strike'],
@@ -27,10 +33,13 @@ export function TextEditor() {
           },
           
         });
-  
-        bindingRef.current = new QuillBinding(yText!, quill)
+        console.log(quill.getModule('cursors'))
+        bindingRef.current = new QuillBinding(yText!, quill, awareness!)
+        console.log(bindingRef.current)
       }
-    }, [yText]);
+    }, [yText, awareness]);
+
+
 
     return(
     <div >
