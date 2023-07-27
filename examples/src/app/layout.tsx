f2abe00dd3a9728d8893b16bdda833ab72c402fc
Felicia <ffeliciachang@gmail.com>
2023-07-27 17:01:48 -0400

    renaming for clarity

--- examples/src/app/layout.tsx
+++ examples/src/app/layout.tsx
@@ -16,10 +16,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <div className="flex flex-col">
         <Link href="/" className="text-xl font-bold text-pink-100 pb-6 px-6">y-sweet</Link>
         <span className="px-6 text-xs tracking-wide text-pink-100 my-2">DEMOS</span>
-        <Link href="/color" className="text-sm text-pink-100 hover:bg-pink-900 transition-all px-8 py-2">Color Grid</Link>
-        <Link href="/todos" className="text-sm text-pink-100 hover:bg-pink-900 transition-all px-8 py-2">To-do List</Link>
+        <Link href="/color-grid" className="text-sm text-pink-100 hover:bg-pink-900 transition-all px-8 py-2">Color Grid</Link>
+        <Link href="/to-do-list" className="text-sm text-pink-100 hover:bg-pink-900 transition-all px-8 py-2">To-do List</Link>
         <Link href="/text-editor" className="text-sm text-pink-100 hover:bg-pink-900 transition-all px-8 py-2">Text Editor</Link>
-        <Link href="/editor" className="text-sm text-pink-100 hover:bg-pink-900 transition-all px-8 py-2">Code Editor</Link>
+        <Link href="/code-editor" className="text-sm text-pink-100 hover:bg-pink-900 transition-all px-8 py-2">Code Editor</Link>
         
         <span className="px-6 text-xs tracking-wide text-pink-100 mt-6 mb-2">LEARN</span>
         <Link href="https://y-sweet.dev" className="text-sm text-pink-100 hover:bg-pink-900 transition-all px-8 py-2">Docs</Link>
--- examples/src/app/page.tsx
+++ examples/src/app/page.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import Title from '@/components/Title'
 
 type HomeProps = {
   searchParams: Record<string, string>
@@ -7,27 +8,25 @@ type HomeProps = {
 export default async function Home({ searchParams }: HomeProps) {
   return (
     <div className="space-y-4 m-10">
-      <h1 className="text-xl font-bold text-neutral-700 pb-4">Demos</h1>
+      <Title>Demos</Title>
       <div className='grid grid-cols-2 gap-4'>
-
-        <Link href="/todos" className="text-neutral-700 hover:bg-red-100 transition-all p-10 border-2 border-black rounded-lg">
-          To Do List
-        </Link>
-
-
-        <Link href="/editor" className="text-neutral-700 hover:bg-red-100 transition-all p-10 border-2 border-black rounded-lg">
-          Code Editor
-        </Link>
-
-        <Link href="/text-editor" className="text-neutral-700 hover:bg-red-100 transition-all p-10 border-2 border-black rounded-lg">
-          Text Editor
-        </Link>
-
-        <Link href="/color" className="text-neutral-700 hover:bg-red-100 transition-all p-10 border-2 border-black rounded-lg">
-          Color Grid
-        </Link>
-
+        <ProjectLink name="Color Grid" url="/color-grid" />
+        <ProjectLink name="To Do List" url="/to-do-list" />
+        <ProjectLink name="Text Editor" url="/text-editor" />
+        <ProjectLink name="Code Editor" url="/code-editor" />
       </div>
     </div>
+  )
+}
+
+interface ProjectLinkProps {
+  name: string
+  url: string
+}
+function ProjectLink(props:ProjectLinkProps) {
+  return(
+    <Link href={props.url} className="text-pink-950 hover:bg-white transition-all p-10 border-2 border-pink-950 rounded-lg">
+    {props.name}
+  </Link>
   )
 }
