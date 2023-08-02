881c0acfc8be10c3ffa11859ea4e45c327f988d3
Paul Butler <paul@driftingin.space>
2023-08-02 10:51:59 -0400

    fix tests hopefully

--- js-pkg/react/src/main.tsx
+++ js-pkg/react/src/main.tsx
@@ -4,9 +4,8 @@ import { createContext, useCallback, useContext, useEffect, useMemo, useState } 
 import type { ReactNode } from 'react'
 import { WebsocketProvider } from 'y-websocket'
 import * as Y from 'yjs'
-import { ClientToken } from '@y-sweet/sdk'
 import type { Awareness } from 'y-protocols/awareness'
-import { createYjsProvider } from './yjs-provider'
+import { ClientToken, createYjsProvider } from './yjs-provider'
 
 type YjsContextType = {
   doc: Y.Doc
