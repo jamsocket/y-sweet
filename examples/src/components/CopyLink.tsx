24c865895c2bc3ce639aba86202467998b4a86cf
Felicia <ffeliciachang@gmail.com>
2023-08-04 11:42:19 -0700

    format

--- examples/src/components/CopyLink.tsx
+++ examples/src/components/CopyLink.tsx
@@ -19,9 +19,7 @@ export default function CopyLink() {
 
   return (
     <div className="pt-6 flex text-left items-center">
-      <span className="text-neutral-400 pr-2">
-        Share this document
-      </span>
+      <span className="text-neutral-400 pr-2">Share this document</span>
       <button
         className="w-24 py-1 rounded-lg bg-neutral-50 border-white border text-neutral-400 transition-all hover:bg-white"
         onClick={copyLinkToClipboard}
