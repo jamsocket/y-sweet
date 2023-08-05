49e7f36a30611de90e5addb55bd94519fa91574a
Paul Butler <paul@driftingin.space>
2023-08-05 14:27:25 -0400

    Make snapshot interval configurable on cloudflare

--- crates/y-sweet-worker/src/config.rs
+++ crates/y-sweet-worker/src/config.rs
@@ -1,9 +1,11 @@
 use serde::{Deserialize, Serialize};
+use std::time::Duration;
 use worker::Env;
 
 const BUCKET: &str = "Y_SWEET_DATA";
 const AUTH_KEY: &str = "AUTH_KEY";
 const USE_HTTPS: &str = "USE_HTTPS";
+const CHECKPOINT_FREQ_SECONDS: &str = "CHECKPOINT_FREQ_SECONDS";
 
 #[derive(Serialize, Deserialize)]
 pub struct Configuration {
@@ -12,6 +14,7 @@ pub struct Configuration {
     pub bucket: String,
     pub bucket_prefix: Option<String>,
     pub url_prefix: Option<String>,
+    pub timeout_interval: Duration,
 }
 
 impl From<&Env> for Configuration {
@@ -21,12 +24,18 @@ impl From<&Env> for Configuration {
             .var(USE_HTTPS)
             .map(|s| s.to_string() != "false")
             .unwrap_or(false);
+        let timeout_interval = Duration::from_secs(
+            env.var(CHECKPOINT_FREQ_SECONDS)
+                .map(|s| s.to_string().parse().expect("Could not parse CHECKPOINT_FREQ_SECONDS as u64"))
+                .unwrap_or(45)
+        );
         Self {
             auth_key,
             use_https,
             bucket: BUCKET.to_string(),
             bucket_prefix: None,
             url_prefix: None,
+            timeout_interval,
         }
     }
 }
