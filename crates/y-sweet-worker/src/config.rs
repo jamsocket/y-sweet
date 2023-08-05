89ed8b03edc02194276d9b8ecbeffe13a1ee9c69
Paul Butler <paul@driftingin.space>
2023-08-05 09:30:02 -0400

    use cloudflare header to determine protocol

--- crates/y-sweet-worker/src/config.rs
+++ crates/y-sweet-worker/src/config.rs
@@ -3,12 +3,10 @@ use worker::Env;
 
 const BUCKET: &str = "Y_SWEET_DATA";
 const AUTH_KEY: &str = "AUTH_KEY";
-const USE_HTTPS: &str = "USE_HTTPS";
 
 #[derive(Serialize, Deserialize)]
 pub struct Configuration {
     pub auth_key: Option<String>,
-    pub use_https: bool,
     pub bucket: String,
     pub bucket_prefix: Option<String>,
     pub url_prefix: Option<String>,
@@ -17,13 +15,8 @@ pub struct Configuration {
 impl From<&Env> for Configuration {
     fn from(env: &Env) -> Self {
         let auth_key = env.var(AUTH_KEY).map(|s| s.to_string()).ok();
-        let use_https = env
-            .var(USE_HTTPS)
-            .map(|s| s.to_string() != "false")
-            .unwrap_or(false);
         Self {
             auth_key,
-            use_https,
             bucket: BUCKET.to_string(),
             bucket_prefix: None,
             url_prefix: None,
