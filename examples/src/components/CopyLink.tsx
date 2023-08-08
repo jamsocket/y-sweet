85d3c2a1766ef61320c2ec32629e56918da9eb5a
Taylor Baldwin <taylorbaldwin@gmail.com>
2023-08-08 14:00:10 -0400

    copy link tweak

--- examples/src/components/CopyLink.tsx
+++ examples/src/components/CopyLink.tsx
@@ -1,5 +1,7 @@
 'use client'
+
 import { useState } from 'react'
+import { LinkIcon } from '@heroicons/react/24/outline'
 
 export default function CopyLink() {
   const [copied, setCopied] = useState(false)
@@ -18,12 +20,13 @@ export default function CopyLink() {
   }
 
   return (
-    <div className="pt-6 flex text-left items-center">
-      <span className="text-neutral-400 pr-2">Share this document</span>
+    <div className="pt-6 flex text-left items-center text-neutral-500">
+      <span className="pr-2">Share this document</span>
       <button
-        className="w-24 py-1 rounded-lg bg-neutral-50 border-white border text-neutral-400 transition-all hover:bg-white"
+        className="flex text-sm items-center gap-1 px-2 py-1 rounded-lg bg-neutral-50 border-white border transition-all hover:bg-white"
         onClick={copyLinkToClipboard}
       >
+        <LinkIcon className="h-3 w-3" />
         {copied ? 'Copied!' : 'Copy Link'}
       </button>
     </div>
