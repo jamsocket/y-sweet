a3879e9856cf1c670e99054e45e7a26ab6dd4743
Abhishek Cherath <abhi@driftingin.space>
2023-08-16 10:59:49 -0400

    time and init bucket

--- crates/y-sweet-worker/src/s3_store.rs
+++ crates/y-sweet-worker/src/s3_store.rs
@@ -2,12 +2,14 @@ use anyhow::Result;
 use async_trait::async_trait;
 use reqwest::{Client, StatusCode};
 use rusty_s3::{Bucket, Credentials, S3Action};
-use std::time::Duration;
+use time::OffsetDateTime;
+use std::{time::Duration, cell::OnceCell};
 use y_sweet_core::store::Store;
 
 const PRESIGNED_URL_DURATION_SECONDS: u64 = 60 * 60;
 pub struct S3Store {
     bucket: Bucket,
+	init_bucket: OnceCell<()>,
     credentials: Credentials,
     prefix: Option<String>,
     presigned_url_duration: Duration,
@@ -22,6 +24,8 @@ impl S3Store {
         aws_secret: String,
     ) -> Self {
         let credentials = Credentials::new(aws_access_key_id, aws_secret);
+		let bucket_name = format!(
+			"{}-{}", prefix.clone().unwrap_or("NA".to_string()), bucket_name);
         let endpoint = format!("https://s3.dualstack.{}.amazonaws.com", region)
             .parse()
             .expect("endpoint is a valid Url");
@@ -29,13 +33,28 @@ impl S3Store {
         let bucket = Bucket::new(endpoint, path_style, bucket_name, region)
             .expect("Url has a valid scheme and host");
 
+		let presigned_url_duration = Duration::from_secs(PRESIGNED_URL_DURATION_SECONDS);
         S3Store {
             bucket,
+			init_bucket: OnceCell::new(),
             credentials,
             prefix,
-            presigned_url_duration: Duration::from_secs(PRESIGNED_URL_DURATION_SECONDS),
+            presigned_url_duration
         }
     }
+
+	async fn init_bucket(&self) -> Result<()> {
+		if self.init_bucket.get().is_none() {
+			let action = self.bucket.create_bucket(&self.credentials);
+			let presigned_url = action.sign_with_time(
+				self.presigned_url_duration, &OffsetDateTime::now_utc());
+			let client = Client::new();
+			let _response = client.put(presigned_url).send().await?;
+			self.init_bucket.set(()).map_err(|_|anyhow::anyhow!("failed to set"))?;
+		}
+
+		self.init_bucket.get().ok_or(anyhow::anyhow!("should be ready!")).map(|_| ())
+	}
 
     fn prefixed_key(&self, key: &str) -> String {
         if let Some(path_prefix) = &self.prefix {
@@ -49,44 +68,56 @@ impl S3Store {
 #[async_trait(?Send)]
 impl Store for S3Store {
     async fn get(&self, key: &str) -> Result<Option<Vec<u8>>> {
+		self.init_bucket().await?;
         let prefixed_key = self.prefixed_key(key);
         let object_get = self
             .bucket
             .get_object(Some(&self.credentials), &prefixed_key);
-        let presigned_url = object_get.sign(self.presigned_url_duration);
+        let presigned_url = object_get.sign_with_time(
+			self.presigned_url_duration, &OffsetDateTime::now_utc());
         let client = Client::new();
         let response = client.get(presigned_url).send().await?;
-        Ok(Some(response.bytes().await?.to_vec()))
+		match response.status() {
+			StatusCode::NOT_FOUND => Ok(None),
+			StatusCode::OK => Ok(Some(response.bytes().await?.to_vec())),
+			_ => Err(anyhow::anyhow!("Not Ok"))
+		}
     }
 
     async fn set(&self, key: &str, value: Vec<u8>) -> Result<()> {
+		self.init_bucket().await?;
         let prefixed_key = self.prefixed_key(key);
         let action = self
             .bucket
             .put_object(Some(&self.credentials), &prefixed_key);
-        let presigned_url = action.sign(self.presigned_url_duration);
+        let presigned_url = action.sign_with_time(
+			self.presigned_url_duration, &OffsetDateTime::now_utc());
         let client = Client::new();
         let _response = client.put(presigned_url).body(value).send().await?;
         Ok(())
     }
 
     async fn remove(&self, key: &str) -> Result<()> {
+		self.init_bucket().await?;
         let prefixed_key = self.prefixed_key(key);
         let action = self
             .bucket
             .delete_object(Some(&self.credentials), &prefixed_key);
-        let presigned_url = action.sign(self.presigned_url_duration);
+        let presigned_url = action.sign_with_time(
+			self.presigned_url_duration, &OffsetDateTime::now_utc());
         let client = Client::new();
         client.delete(presigned_url).send().await?;
         Ok(())
     }
 
     async fn exists(&self, key: &str) -> Result<bool> {
+		self.init_bucket().await?;
         let prefixed_key = self.prefixed_key(key);
         let action = self
             .bucket
             .head_object(Some(&self.credentials), &prefixed_key);
-        let presigned_url = action.sign(self.presigned_url_duration);
+        let presigned_url = action.sign_with_time(
+			self.presigned_url_duration, &OffsetDateTime::now_utc());
         let client = Client::new();
         //this should only return false if 404? I think?
         let res_status = client.head(presigned_url).send().await?.status();
