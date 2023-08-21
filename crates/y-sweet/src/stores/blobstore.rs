28c0bfc88d5c6beafa8eff67429729f533d4fde4
Abhishek Cherath <abhi@driftingin.space>
2023-08-21 16:53:13 -0400

    inprog switch to rusty-s3

--- crates/y-sweet/src/stores/blobstore.rs
+++ crates/y-sweet/src/stores/blobstore.rs
@@ -1,37 +1,111 @@
 use anyhow::Result;
 use async_trait::async_trait;
-use s3::bucket::Bucket;
-use s3::creds::Credentials;
-use s3::error::S3Error;
-use s3::region::Region;
+use std::{cell::RefCell, sync::{Arc, RwLock}, time::Duration};
+use reqwest::{Client, StatusCode, Response};
+use rusty_s3::{Bucket, Credentials, S3Action};
+use tokio::sync::Mutex;
 use y_sweet_core::store::Store;
 
+
+const PRESIGNED_URL_DURATION_SECONDS: u64 = 60 * 60;
 pub struct S3Store {
     bucket: Bucket,
-    prefix: String,
+    _bucket_inited: Arc<Mutex<bool>>,
+    client: Client,
+    credentials: Credentials,
+    prefix: Option<String>,
+    presigned_url_duration: Duration,
 }
 
 impl S3Store {
-    pub fn new(region: Region, bucket: String, prefix: String) -> Result<Self, anyhow::Error> {
-        let credentials = Credentials::default()?;
-        let bucket = Bucket::new(&bucket, region, credentials)?;
-        Ok(Self { bucket, prefix })
+    pub fn new(
+        region: String,
+        bucket_name: String,
+        prefix: Option<String>,
+        aws_access_key_id: String,
+        aws_secret: String,
+    ) -> Self {
+        let credentials = Credentials::new(aws_access_key_id, aws_secret);
+        let endpoint = format!("https://s3.dualstack.{}.amazonaws.com", region)
+            .parse()
+            .expect("endpoint is a valid Url");
+        let path_style = rusty_s3::UrlStyle::VirtualHost;
+        let bucket = Bucket::new(endpoint, path_style, bucket_name, region)
+            .expect("Url has a valid scheme and host");
+        let client = Client::new();
+
+        let presigned_url_duration = Duration::from_secs(PRESIGNED_URL_DURATION_SECONDS);
+        S3Store {
+            bucket,
+            _bucket_inited: Arc::new(Mutex::new(false)),
+            client,
+            credentials,
+            prefix,
+            presigned_url_duration,
+        }
     }
 
-    fn make_key(&self, key: &str) -> String {
-        format!("{}/{}", &self.prefix, key)
+	async fn head_bucket(&self) -> Result<Response> {
+		let action = self.bucket.head_bucket(Some(&self.credentials));
+		let presigned_url =
+			action.sign(self.presigned_url_duration);
+		let response = self.client.head(presigned_url).send().await?;
+		Ok(response)
+	}
+
+	//lazily checks bucket exists on first use
+    async fn inited_bucket(&self) -> Result<&Bucket> {
+		let mut bucket_inited = self._bucket_inited.lock().await;
+        if !*bucket_inited {
+			let response = self.head_bucket().await?;
+            match response.status() {
+                StatusCode::OK => {
+					*bucket_inited = true;
+                }
+                StatusCode::NOT_FOUND => {
+                    return Err(anyhow::anyhow!(
+                        "No such bucket {} exists!",
+                        self.bucket.name()
+                    ))
+                }
+                _ => {
+                    return Err(anyhow::anyhow!(
+                        "Other AWS Error: Code {} Err {}",
+						response.status(),
+						response.text().await?
+                    ))
+                }
+            }
+        }
+        Ok(&self.bucket)
+    }
+
+    fn prefixed_key(&self, key: &str) -> String {
+        if let Some(path_prefix) = &self.prefix {
+            format!("{}/{}", path_prefix, key)
+        } else {
+            key.to_string()
+        }
     }
 }
 
 #[async_trait]
 impl Store for S3Store {
     async fn get(&self, key: &str) -> Result<Option<Vec<u8>>> {
-        let response = self.bucket.get_object(&self.make_key(key)).await;
-
-        match response {
-            Ok(result) => Ok(Some(result.to_vec())),
-            Err(S3Error::Http(404, _)) => Ok(None),
-            Err(e) => Err(e.into()),
+        let bucket = self.inited_bucket().await?;
+        let prefixed_key = self.prefixed_key(key);
+        let object_get = bucket.get_object(Some(&self.credentials), &prefixed_key);
+        let presigned_url =
+            object_get.sign(self.presigned_url_duration);
+        let response = self.client.get(presigned_url).send().await?;
+        match response.status() {
+            StatusCode::NOT_FOUND => Ok(None),
+            StatusCode::OK => Ok(Some(response.bytes().await?.to_vec())),
+            _ => Err(anyhow::anyhow!(
+                "Other AWS Error: Code {} Err {}",
+                response.status(),
+                response.text().await?
+            )),
         }
     }
 
