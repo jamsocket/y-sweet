bc881a3f776ca7810539f7f0cb93a25c324b475b
Paul Butler <paul@driftingin.space>
2023-09-03 13:50:32 -0400

    Fix build issue (#105)
    * Fix module resolution
    
    * base64 conversion
    
    * add @types/node
    
    * format

--- js-pkg/sdk/src/main.ts
+++ js-pkg/sdk/src/main.ts
@@ -240,9 +240,33 @@ export async function createDoc(serverToken?: ServerToken | string): Promise<Doc
   return await manager.createDoc()
 }
 
+function stringToBase64(input: string) {
+  if (typeof window !== 'undefined' && window.btoa) {
+    // Browser
+    return window.btoa(input)
+  } else if (typeof Buffer !== 'undefined') {
+    // Node.js
+    return Buffer.from(input).toString('base64')
+  } else {
+    throw new Error('Unable to encode to Base64')
+  }
+}
+
+function base64ToString(input: string) {
+  if (typeof window !== 'undefined' && window.atob) {
+    // Browser
+    return window.atob(input)
+  } else if (typeof Buffer !== 'undefined') {
+    // Node.js
+    return Buffer.from(input, 'base64').toString()
+  } else {
+    throw new Error('Unable to decode from Base64')
+  }
+}
+
 export function encodeClientToken(token: ClientToken): string {
   const jsonString = JSON.stringify(token)
-  let base64 = Buffer.from(jsonString).toString('base64')
+  let base64 = stringToBase64(jsonString)
   base64 = base64.replace('+', '-').replace('/', '_').replace(/=+$/, '')
   return base64
 }
@@ -252,6 +276,6 @@ export function decodeClientToken(token: string): ClientToken {
   while (base64.length % 4) {
     base64 += '='
   }
-  const jsonString = Buffer.from(base64, 'base64').toString('utf8')
+  const jsonString = base64ToString(base64)
   return JSON.parse(jsonString)
 }
