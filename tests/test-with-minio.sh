afbf75b9b734ad896807811c4698643ce3a7e9bc
Paul Butler <paul@driftingin.space>
2023-10-21 10:57:00 -0400

    newlines

--- tests/test-with-minio.sh
+++ tests/test-with-minio.sh
@@ -8,10 +8,17 @@ docker rm -f minio-mc
 docker run -d --network host --name minio \
   quay.io/minio/minio server /data --console-address ":9001"
 
-docker run -d --network host --name minio-mc --entrypoint=/bin/sh minio/mc -c 'while true; do sleep 1000; done'
-docker exec minio-mc mc alias set mycloud http://localhost:9000 minioadmin minioadmin
-docker exec minio-mc mc mb mycloud/ysweet-testing-y-sweet-data
+docker run -d \
+  --network host \
+  --name minio-mc \
+  --entrypoint=/bin/sh minio/mc \
+  -c 'while true; do sleep 1000; done'
 
+docker exec minio-mc mc \
+  alias set mycloud http://localhost:9000 minioadmin minioadmin
+
+docker exec minio-mc mc \
+  mb mycloud/ysweet-testing-y-sweet-data
 
 export Y_SWEET_S3_BUCKET_PREFIX=testing
 export Y_SWEET_S3_BUCKET_NAME=ysweet-testing-y-sweet-data
