ab7f245b67d4eced65f64aace45fe96ea4421fa5
Felicia <ffeliciachang@gmail.com>
2023-11-06 12:59:40 -0800

    format

--- examples/src/app/layout.tsx
+++ examples/src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <body className="bg-[radial-gradient(at_bottom_left,_var(--tw-gradient-stops))] from-pink-50 to-pink-900">
         <Sidebar>
-          <CopyLink/>
+          <CopyLink />
           <div className="overflow-auto absolute w-full h-full top-0 pt-14 lg:relative lg:w-auto lg:pt-0 bg-[radial-gradient(at_bottom_left,_var(--tw-gradient-stops))] from-white/90 via-pink-50/90 to-pink-100/90 sm:mr-2 sm:rounded-lg">
             {children}
           </div>
--- examples/src/components/CopyLink.tsx
+++ examples/src/components/CopyLink.tsx
@@ -12,25 +12,28 @@ export default function CopyLink() {
     const currentPageURL = window.location.href
 
     try {
-      window.open(currentPageURL, '_blank');
+      window.open(currentPageURL, '_blank')
     } catch (err) {
-      console.error('Failed to copy: ', err);
+      console.error('Failed to copy: ', err)
     }
   }
 
-  if(hideCallout) {
+  if (hideCallout) {
     return <></>
   }
 
   return (
     <div className="mr-2 mb-2 text-left items-center text-neutral-500 border-2 border-yellow-200 rounded-lg bg-yellow-50 p-6 ">
       <div className="flex justify-between pb-2">
-      <Title>How to collaborate on this document</Title>
-      <button onClick={() => setHideCallout(true)}>
-        <XMarkIcon className="h-5 w-5 hover:text-black"/>
-      </button>
+        <Title>How to collaborate on this document</Title>
+        <button onClick={() => setHideCallout(true)}>
+          <XMarkIcon className="h-5 w-5 hover:text-black" />
+        </button>
       </div>
-      <span className="pr-2">To simulate another user appearing on this document, copy the link to this document and open it in a new window. When you interact on one screen, you should see the user action happens on another.
+      <span className="pr-2">
+        To simulate another user appearing on this document, copy the link to this document and open
+        it in a new window. When you interact on one screen, you should see the user action happens
+        on another.
       </span>
       <button
         className="flex items-center gap-1 px-4 py-2 mt-4 rounded-lg bg-pink-950 text-white border transition-all "
