2b3d08413a49736dcd7f518c3bcb588739cf5d24
Felicia <ffeliciachang@gmail.com>
2023-11-07 14:06:45 -0800

    reduce vertical height

--- examples/src/components/CopyLink.tsx
+++ examples/src/components/CopyLink.tsx
@@ -6,7 +6,6 @@ import { ArrowUpRightIcon, XMarkIcon } from '@heroicons/react/24/outline'
 import Title from '@/components/Title'
 
 export default function CopyLink() {
-  const [hideCallout, setHideCallout] = useState(false)
 
   const openLinkInNewTab = async () => {
     const currentPageURL = window.location.href
@@ -17,31 +16,28 @@ export default function CopyLink() {
       console.error('Failed to copy: ', err)
     }
   }
-
-  if (hideCallout) {
-    return <></>
-  }
+  //exclude from whiteboard, and toc
+  // make it so you cant x it out, reduce vertical space
 
   return (
-    <div className="mr-2 mb-2 text-left items-center text-neutral-500 border-2 border-yellow-200 rounded-lg bg-yellow-50 p-6 ">
-      <div className="flex justify-between pb-2">
-        <Title>How to collaborate on this document</Title>
-        <button onClick={() => setHideCallout(true)}>
-          <XMarkIcon className="h-5 w-5 hover:text-black" />
-        </button>
-      </div>
-      <span className="pr-2">
-        To simulate another user appearing on this document, copy the link to this document and open
-        it in a new window. When you interact on one screen, you should see the user action happens
-        on another.
-      </span>
-      <button
-        className="flex items-center gap-1 px-4 py-2 mt-4 rounded-lg bg-pink-950 text-white border transition-all "
+    <div className="mr-2 mb-2 text-left items-center text-neutral-500 border-2 border-yellow-200 rounded-lg bg-yellow-50 px-6 py-3">
+      <div className="flex justify-between items-center pb-1">
+        <Title>Collaborate on this document</Title>
+        <div className="flex">
+        <button
+        className="text-sm flex items-center gap-1 px-3 py-1 rounded-lg bg-pink-950 text-white border transition-all "
         onClick={openLinkInNewTab}
       >
         Open link in a new tab
-        <ArrowUpRightIcon className="h-5 w-5 " />
+        <ArrowUpRightIcon className="h-4 w-4 font-bold" />
       </button>
+        </div>
+      </div>
+      <div className="pr-2 w-full md:w-3/4">
+        To simulate another user on this document, open the same link in a new tab. When you interact, you should see the user action happen
+        in both windows.
+      </div>
+
     </div>
   )
 }
