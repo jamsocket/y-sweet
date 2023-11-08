3e53090f3cc1c097eb2d114e12b8f07d3f2ff667
Felicia <ffeliciachang@gmail.com>
2023-11-08 12:51:57 -0800

    format

--- examples/src/components/CopyLink.tsx
+++ examples/src/components/CopyLink.tsx
@@ -4,7 +4,6 @@ import { ArrowUpRightIcon } from '@heroicons/react/24/outline'
 import Title from '@/components/Title'
 
 export default function CopyLink() {
-
   return (
     <div className="mb-2 text-left items-center text-neutral-500 border-2 border-yellow-200 rounded-lg bg-yellow-50 px-6 py-3">
       <div className="flex justify-between items-center pb-1">
@@ -12,7 +11,7 @@ export default function CopyLink() {
         <div className="flex">
           <button
             className="text-sm flex items-center gap-1 px-3 py-1 rounded-lg bg-pink-950 text-white border transition-all "
-            onClick={() => window.open(window.location.href, "_blank")}
+            onClick={() => window.open(window.location.href, '_blank')}
           >
             Open link in a new tab
             <ArrowUpRightIcon className="h-4 w-4 font-bold" />
