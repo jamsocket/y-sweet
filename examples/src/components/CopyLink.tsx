d09d28923ef19e6be7015f91fbdfa357fc78bbe4
Felicia <ffeliciachang@gmail.com>
2023-11-08 12:51:09 -0800

    moving back to button

--- examples/src/components/CopyLink.tsx
+++ examples/src/components/CopyLink.tsx
@@ -1,26 +1,22 @@
 'use client'
 import { ArrowUpRightIcon } from '@heroicons/react/24/outline'
-import { usePathname, useSearchParams } from 'next/navigation'
 
 import Title from '@/components/Title'
 
 export default function CopyLink() {
-  const pathname = usePathname()
-  const params = useSearchParams()
 
   return (
     <div className="mb-2 text-left items-center text-neutral-500 border-2 border-yellow-200 rounded-lg bg-yellow-50 px-6 py-3">
       <div className="flex justify-between items-center pb-1">
         <Title>Collaborate on this document</Title>
         <div className="flex">
-          <a
+          <button
             className="text-sm flex items-center gap-1 px-3 py-1 rounded-lg bg-pink-950 text-white border transition-all "
-            href={`${pathname}?doc=${params.get('doc')}`}
-            target="_blank"
+            onClick={() => window.open(window.location.href, "_blank")}
           >
             Open link in a new tab
             <ArrowUpRightIcon className="h-4 w-4 font-bold" />
-          </a>
+          </button>
         </div>
       </div>
       <div className="pr-2 w-full md:w-3/4">
