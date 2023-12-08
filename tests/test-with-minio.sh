eeef6890115f796b1e7dbffe6fe93b6f96326e03
Paul Butler <paul@driftingin.space>
2023-12-08 16:07:23 -0500

    Make Minio tests work on Mac

--- tests/test-with-minio.sh
+++ tests/test-with-minio.sh
@@ -5,17 +5,19 @@ set -e
 docker rm -f minio
 docker rm -f minio-mc
 
-docker run -d --network host --name minio \
+docker run -d --name minio \
+  -p 9000:9000 \
   quay.io/minio/minio server /data --console-address ":9001"
 
+# Start a container with a sleep loop to run the minio client.
 docker run -d \
-  --network host \
   --name minio-mc \
+  --add-host "host.docker.internal:host-gateway" \
   --entrypoint=/bin/sh minio/mc \
   -c 'while true; do sleep 1000; done'
 
 docker exec minio-mc mc \
-  alias set mycloud http://localhost:9000 minioadmin minioadmin
+  alias set mycloud http://host.docker.internal:9000 minioadmin minioadmin
 
 docker exec minio-mc mc \
   mb mycloud/ysweet-testing-y-sweet-data
