7086c85c17fbe260fe9d28a666f08c966b6fa4af
Paul Butler <paul@driftingin.space>
2024-04-17 07:45:56 -0400

    add comment and hard-code config

--- crates/y-sweet-worker/src/config.rs
+++ crates/y-sweet-worker/src/config.rs
@@ -7,13 +7,22 @@ use y_sweet_core::store::s3::S3Config;
 const BUCKET: &str = "Y_SWEET_DATA";
 const BUCKET_KIND: &str = "BUCKET_KIND";
 const AUTH_KEY: &str = "AUTH_KEY";
-const CHECKPOINT_FREQ_SECONDS: &str = "CHECKPOINT_FREQ_SECONDS";
 const S3_ACCESS_KEY_ID: &str = "AWS_ACCESS_KEY_ID";
 const S3_SECRET_ACCESS_KEY: &str = "AWS_SECRET_ACCESS_KEY";
 const S3_REGION: &str = "AWS_REGION";
 const S3_ENDPOINT: &str = "AWS_ENDPOINT_URL_S3";
 const S3_BUCKET_PREFIX: &str = "S3_BUCKET_PREFIX";
 const S3_BUCKET_NAME: &str = "S3_BUCKET_NAME";
+
+// Note: unlike the native server, the worker checkpoint frequency is not configurable because
+// it directly relates to Cloudflare platform configuration. Per their docs:
+//
+//     A Durable Object is given 30 seconds of additional CPU time for every request it processes,
+//     including WebSocket messages. In the absence of failures, in-memory state should not be
+//     reset after less than 30 seconds of inactivity.
+//
+// Ref: https://ts.cloudflare.community/workers/runtime-apis/durable-objects/#in-memory-state
+const DEFAULT_CHECKPOINT_FREQ_SECONDS: u64 = 29;
 
 #[derive(Serialize, Deserialize)]
 pub enum BucketKind {
@@ -80,15 +89,7 @@ impl TryFrom<&Env> for Configuration {
 
     fn try_from(env: &Env) -> Result<Self, Self::Error> {
         let auth_key = env.var(AUTH_KEY).map(|s| s.to_string()).ok();
-        let timeout_interval = Duration::from_secs(
-            env.var(CHECKPOINT_FREQ_SECONDS)
-                .map(|s| {
-                    s.to_string()
-                        .parse()
-                        .expect("Could not parse CHECKPOINT_FREQ_SECONDS as u64")
-                })
-                .unwrap_or(9),
-        );
+        let timeout_interval = Duration::from_secs(DEFAULT_CHECKPOINT_FREQ_SECONDS);
 
         let bucket_kind = env.var(BUCKET_KIND).map_or_else(
             |_| Ok(BucketKind::NativeR2),
