50ef092becee545681fcf282db0ea7ab50c5c700
Paul Butler <paul@driftingin.space>
2024-08-22 10:07:02 -0400

    Allow createYjsProvider to take a callback function to generate a client token

--- js-pkg/client/src/main.ts
+++ js-pkg/client/src/main.ts
@@ -3,8 +3,28 @@ import * as Y from 'yjs'
 import { ClientToken, encodeClientToken } from '@y-sweet/sdk'
 export { YSweetProvider, YSweetProviderParams }
 
+type RelaxedClientToken = ClientToken | (() => ClientToken) | (() => Promise<ClientToken>)
+
+async function resolveClientToken(relaxedClientToken: RelaxedClientToken): Promise<ClientToken> {
+  if (typeof relaxedClientToken === 'function') {
+    let result: ClientToken | Promise<ClientToken> = relaxedClientToken()
+    if (result instanceof Promise) {
+      return await result
+    } else {
+      return result
+    }
+  } else {
+    return relaxedClientToken
+  }
+}
+
 /**
- * Given a {@link ClientToken}, create a {@link YSweetProvider} for it.
+ * Given a {@link ClientToken}, create a (wrapped) {@link YSweetProvider} for it.
+ *
+ * Like {@link YSweetProvider}, the wrapped provider supports `destroy()` to clean up resources.
+ *
+ * Instead of passing a {@link ClientToken} directly, you can also pass a function that
+ * returns a {@link ClientToken}, or returns a {@link Promise} that resolves to a {@link ClientToken}.
  *
  * @param doc
  * @param clientToken
@@ -13,17 +33,36 @@ export { YSweetProvider, YSweetProviderParams }
  */
 export function createYjsProvider(
   doc: Y.Doc,
-  clientToken: ClientToken,
+  clientToken: RelaxedClientToken,
   extraOptions: Partial<YSweetProviderParams> = {},
-): YSweetProvider {
-  const params = clientToken.token ? { token: clientToken.token } : undefined
+): LazyProvider {
+  return new LazyProvider(doc, clientToken, extraOptions)
+}
 
-  const provider = new YSweetProvider(clientToken.url, clientToken.docId, doc, {
-    params,
-    ...extraOptions,
-  })
+class LazyProvider {
+  provider: Promise<YSweetProvider>
 
-  return provider
+  constructor(
+    doc: Y.Doc,
+    relaxedClientToken: RelaxedClientToken,
+    extraOptions: Partial<YSweetProviderParams>,
+  ) {
+    this.provider = new Promise(async (resolve) => {
+      let clientToken = await resolveClientToken(relaxedClientToken)
+
+      const params = clientToken.token ? { token: clientToken.token } : undefined
+      const provider = new YSweetProvider(clientToken.url, clientToken.docId, doc, {
+        params,
+        ...extraOptions,
+      })
+
+      resolve(provider)
+    })
+  }
+
+  destroy() {
+    this.provider.then((provider) => provider.destroy())
+  }
 }
 
 /**
