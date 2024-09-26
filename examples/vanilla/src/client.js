139aa161f3cd5e2feff9feb605711822041fee93
Taylor Baldwin <taylorbaldwin@gmail.com>
2024-09-26 17:01:03 -0400

    refactor vanillajs demo to work with new ysweetprovider

--- examples/vanilla/src/client.js
+++ examples/vanilla/src/client.js
@@ -10,27 +10,16 @@ async function main() {
   // First, fetch a client token that can access the docId in the URL.
   // Or, if the URL does not contain a docId, get a client token for a new doc.
   const searchParams = new URLSearchParams(window.location.search)
-  const docId = searchParams.get(QUERY_PARAM)
+  const docId = searchParams.get(QUERY_PARAM) ?? Math.random().toString(36).substring(2, 15)
 
-  const body = docId ? JSON.stringify({ docId }) : null
-  console.log('body', body)
-  const res = await fetch('http://localhost:9090/y-sweet-auth', {
-    method: 'POST',
-    body,
-    headers: { 'Content-Type': 'application/json' },
-  })
-  const clientToken = await res.json()
-
-  // Update the URL to include the docId if it was not already present.
-  if (!docId) {
-    const url = new URL(window.location.href)
-    url.searchParams.set(QUERY_PARAM, clientToken.docId)
-    window.history.replaceState({}, '', url.toString())
-  }
+  // Update the URL to include the docId in case it's not already present.
+  const url = new URL(window.location.href)
+  url.searchParams.set(QUERY_PARAM, docId)
+  window.history.replaceState({}, '', url.toString())
 
   // Create a Yjs document and connect it to the Y-Sweet server.
   const doc = new Y.Doc()
-  createYjsProvider(doc, clientToken, { disableBc: true })
+  createYjsProvider(doc, docId, 'http://localhost:9090/y-sweet-auth')
   const sharedColorMap = doc.getMap('colorgrid')
 
   // Create the UI for the color grid.
