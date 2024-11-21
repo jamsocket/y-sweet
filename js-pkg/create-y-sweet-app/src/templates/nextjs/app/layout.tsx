10f6cbd7c12fdd1a6116c32d868df959b0824361
Jake Lazaroff <jake@jamsocket.com>
2024-11-21 17:20:38 -0500

    Improve fonts

--- js-pkg/create-y-sweet-app/src/templates/nextjs/app/layout.tsx
+++ js-pkg/create-y-sweet-app/src/templates/nextjs/app/layout.tsx
@@ -2,7 +2,6 @@ import { Inter } from "next/font/google";
 
 import "./globals.css";
 
-// If loading a variable font, you don't need to specify the font weight
 const inter = Inter({
   subsets: ["latin"],
   display: "swap",
--- js-pkg/create-y-sweet-app/src/templates/remix/app/root.tsx
+++ js-pkg/create-y-sweet-app/src/templates/remix/app/root.tsx
@@ -18,7 +18,7 @@ export const links: LinksFunction = () => [
   },
   {
     rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
+    href: "https://fonts.googleapis.com/css2?family=Inter:opsz,wght@14..32,100..900&display=swap",
   },
 ];
 
