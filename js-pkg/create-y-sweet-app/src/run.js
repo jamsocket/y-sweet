19278dd86f2109a0a141b096913a7a7af3df13e5
Jake Lazaroff <jake@jamsocket.com>
2024-11-21 17:39:24 -0500

    Turn CLI into wizard

--- js-pkg/create-y-sweet-app/src/run.js
+++ js-pkg/create-y-sweet-app/src/run.js
@@ -3,6 +3,20 @@ import { cp, readdir } from 'node:fs/promises'
 import { dirname, resolve } from 'node:path'
 import { fileURLToPath } from 'node:url'
 import { parseArgs } from 'node:util'
+import readline from 'node:readline'
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+})
+
+/**
+ * @param {string} prompt
+ * @returns {Promise<string>}
+ */
+function question(prompt, defaultValue = '') {
+  return new Promise((resolve) => rl.question(prompt, (result) => resolve(result || defaultValue)))
+}
 
 const { values, positionals } = parseArgs({ options: { template: { type: 'string', short: 't' } } })
 
@@ -27,7 +41,13 @@ async function init(template, project) {
     process.exit(1)
   }
 
-  const name = project || 'my-y-sweet-app'
+  let name = project
+  if (!name)
+    name = await question(
+      bold('What do you want to call your app? ') + gray('(my-y-sweet-app) '),
+      'my-y-sweet-app',
+    )
+
   const __dirname = dirname(fileURLToPath(import.meta.url))
 
   const src = resolve(__dirname, './templates', template)
@@ -37,13 +57,29 @@ async function init(template, project) {
   try {
     const contents = await readdir(dest)
     if (contents.length > 0) {
-      console.error('Directory ${name} already exists and is not empty.')
+      console.error(`Directory ${name} already exists and is not empty.`)
       process.exit(1)
     }
   } catch (err) {
     // directory doesn't exist, which is fine
     if (err.code !== 'ENOENT') throw err
   }
+
+  let install = false
+  const installResponse = await question(
+    bold('Do you want to install dependencies with npm? ') + gray('(Y/n) '),
+    'y',
+  )
+  if (installResponse.toLowerCase() === 'y') install = true
+
+  let git = false
+  const gitResponse = await question(
+    bold('Do you want to initialize a git repository? ') + gray('(Y/n) '),
+    'y',
+  )
+  if (gitResponse.toLowerCase() === 'y') git = true
+
+  rl.close()
 
   // copy the template files
   try {
@@ -53,11 +89,31 @@ async function init(template, project) {
     process.exit(1)
   }
 
-  // initialize a git repository
-  exec('git init', { cwd: dest })
+  if (git) {
+    // initialize a git repository
+    exec('git init', { cwd: dest })
+  }
 
-  // install dependencies
-  exec('npm install', { cwd: dest })
+  if (install) {
+    // install dependencies
+    exec('npm install', { cwd: dest })
+  }
 
   console.log(`Created y-sweet app in ${name}!`)
+}
+
+/**
+ * Returns a string wrapped in ANSI escape codes for gray text.
+ * @param {string} text
+ */
+function gray(text) {
+  return `\x1b[90m${text}\x1b[0m`
+}
+
+/**
+ * Returns a string wrapped in ANSI escape codes for bold text.
+ * @param {string} text
+ */
+function bold(text) {
+  return `\x1b[1m${text}\x1b[0m`
 }
