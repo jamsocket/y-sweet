d54941a745f1d4e2f83eb7627a7d0f2a61a9b90d
Paul Butler <paul@driftingin.space>
2024-12-16 15:47:32 -0500

    nonce length to const

--- js-pkg/client/src/encryption.ts
+++ js-pkg/client/src/encryption.ts
@@ -4,6 +4,8 @@ const ALGORITHM: AesKeyGenParams = {
 }
 const KEY_USAGE: KeyUsage[] = ['encrypt', 'decrypt']
 
+const NONCE_LENGTH = 12
+
 function arrayBufferToBase64(buffer: ArrayBuffer): string {
   return btoa(String.fromCharCode(...new Uint8Array(buffer)))
 }
@@ -29,7 +31,7 @@ export function generateEncryptionKey(): Promise<CryptoKey> {
 }
 
 export async function encryptData(data: Uint8Array, key: CryptoKey): Promise<Uint8Array> {
-  const iv = crypto.getRandomValues(new Uint8Array(12))
+  const iv = crypto.getRandomValues(new Uint8Array(NONCE_LENGTH))
   const encrypted = await crypto.subtle.encrypt({ name: ALGORITHM.name, iv }, key, data)
 
   const result = new Uint8Array(iv.length + encrypted.byteLength)
@@ -39,8 +41,8 @@ export async function encryptData(data: Uint8Array, key: CryptoKey): Promise<Uin
 }
 
 export async function decryptData(encryptedData: Uint8Array, key: CryptoKey): Promise<Uint8Array> {
-  const iv = encryptedData.slice(0, 12)
-  const data = encryptedData.slice(12)
+  const iv = encryptedData.slice(0, NONCE_LENGTH)
+  const data = encryptedData.slice(NONCE_LENGTH)
   const decrypted = await crypto.subtle.decrypt({ name: ALGORITHM.name, iv }, key, data)
   return new Uint8Array(decrypted)
 }
